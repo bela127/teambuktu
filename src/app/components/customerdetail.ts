d784f7a749c04f012c25e06b334d55d31d89a9d8
Matthieu Laqua <mlmatthieul@gmail.com>
2018-11-19 06:13:09 +0100

    Update customerdetail view

--- src/app/components/customerdetail.ts
+++ src/app/components/customerdetail.ts
@@ -1,4 +1,9 @@
 import {Component} from "@angular/core";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {SessionService} from "../services/SessionService";
+import {DataService} from "../services/DataService";
+import {Customer} from "../model/Customer";
+import {Appointment} from "../model/Appointment";
 
 @Component({
   selector: 'customerdetail-component',
@@ -6,4 +11,23 @@ import {Component} from "@angular/core";
 })
 export class CustomerdetailComponent {
 
+  private customer: Customer;
+
+  constructor(private route: ActivatedRoute, private session: SessionService, private router: Router, private dataService: DataService) {
+    this.session.checkLogin(router);
+  }
+
+  getCustomer(): Customer {
+    return this.customer;
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      let id = params['id'];
+      console.log(params)
+      console.log("ID: " + id)
+      this.customer = this.dataService.getCustomer(id);
+      console.log(this.customer)
+    });
+  }
 }
