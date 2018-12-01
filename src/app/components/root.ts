42d875a229e9d9fc8b06d52a97a147fc3a3a8d53
Matthieu Laqua <mlmatthieul@gmail.com>
2018-12-01 08:38:17 +0100

    Resolve some ts warnings about unused Promises

--- src/app/components/root.ts
+++ src/app/components/root.ts
@@ -1,6 +1,4 @@
 import {Component, ViewChild} from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
 import {Router} from "@angular/router";
 import {SessionService} from '../services/SessionService';
 import {SidenavService} from '../services/SidenavService';
@@ -17,9 +15,10 @@ export class RootComponent {
   @ViewChild('sidenav') public sidenav: MatSidenav;
 
   constructor(private router: Router, private session: SessionService, private sidenavService: SidenavService, private dataservice: DataService) {
-    console.log("Root cons")
-    session.setRouter(router)
+    console.log("Root cons");
+    session.setRouter(router);
     router.navigate(["/login"])
+      .finally(() => console.log("Navigated to '/login', after initialization."));
   }
 
   ngOnInit(): void {
@@ -29,7 +28,9 @@ export class RootComponent {
   public logout(): void {
     this.session.logout();
     this.dataservice.setUserData(null);
-    this.sidenavService.close();
-    this.router.navigate(["/login"]);
+    this.sidenav.close()
+      .finally(() => console.log("Sidenav finally closed."));
+    this.router.navigate(["/login"])
+      .finally(() => console.log("Navigated to '/login', after logout."));
   }
 }
