d0394767862fc83187968bd4fe5af0097c6a7ba1
PeterKaffeeKrieger <peter.krieger@posteo.de>
2018-12-02 22:41:58 +0100

    sds
    dv

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -21,11 +21,10 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Electron'],
-    singleRun: false
+    singleRun: true
   });
 };
