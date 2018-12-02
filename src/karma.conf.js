5ec2ed5b8d106a665be95d32abd25db95dfe4569
PeterKaffeeKrieger <peter.krieger@posteo.de>
2018-12-02 22:33:44 +0100

    avsdv
    sdvsdv

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: true
+    browsers: ['Electron'],
+    singleRun: false
   });
 };
