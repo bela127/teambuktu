52106d28234b929c733c6bd078666dce8d543b5e
PeterKaffeeKrieger <peter.krieger@posteo.de>
2018-12-02 19:32:18 +0100

    sdvsdv
    sdvsdv

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -46,6 +46,13 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine', 'requirejs', '@angular-devkit/build-angular'],
 
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
 
     // list of files / patterns to load in the browser
     files: [
