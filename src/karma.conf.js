e6d19d002c09d2418a0e4c8dc2cb72aa749125a6
Moritz Goeckel <moritz.goeckel@gmail.com>
2018-12-16 09:40:31 +0100

    Trying to get coverage to work

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -20,9 +20,16 @@ module.exports = function (config) {
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
+      combineBrowserReports: true
     },
-    reporters: ['progress', 'kjhtml', 'dots', 'junit'], //'dots', 'junit'
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'src/**/*.js': ['coverage']
+    },
+    reporters: ['progress', 'kjhtml', 'dots', 'junit', 'coverage'], //'dots', 'junit', 'coverage'
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
