d5a72ae3b9358c66acbd6eda47bde5d9ad40bfcc
Moritz Goeckel <moritz.goeckel@gmail.com>
2018-12-16 09:47:59 +0100

    Working on converage

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -12,7 +12,8 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('karma-junit-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-coverage')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
