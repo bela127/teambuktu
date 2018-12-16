f58fcc77bc6de1eeae255ea742bee56bb451c285
Moritz Goeckel <moritz.goeckel@gmail.com>
2018-12-16 22:19:01 +0100

    Trying to get coverage to work

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
       // source files, that you wanna generate coverage for
       // do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      'dist/out-tsc/**/*.js': ['coverage']
+      '**/*.js': ['coverage']
     },
     reporters: ['progress', 'kjhtml', 'dots', 'junit', 'coverage'], //'dots', 'junit', 'coverage'
     port: 9876,
