652c05e8a3fb39aa43b4aa75361cac4227d58e8c
Matthieu Laqua <mlmatthieul@gmail.com>
2019-01-18 22:07:59 +0100

    Update device detail view: add select control for customer

--- src/app/components/device-detail/device-detail.component.ts
+++ src/app/components/device-detail/device-detail.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Device} from "../../container/device";
 import {DeviceService} from "../../services/device.service";
+import {CustomerService} from "../../services/customer.service";
+import {Customer} from "../../container/customer";
 
 @Component({
   selector: 'app-device-detail',
@@ -11,8 +13,10 @@ import {DeviceService} from "../../services/device.service";
 export class DeviceDetailComponent implements OnInit {
 
   private device: Device;
+  private allCustomers: Customer[];
 
   constructor(private deviceService: DeviceService,
+              private customerService: CustomerService,
               private route: ActivatedRoute) {
   }
 
@@ -20,6 +24,9 @@ export class DeviceDetailComponent implements OnInit {
     let id = +this.route.snapshot.paramMap.get("id");
     this.deviceService.getDevice(id)
       .subscribe(device => this.device = device);
+
+    this.customerService.getCustomers()
+      .subscribe(customers => this.allCustomers = customers);
   }
 
   save(): void {
