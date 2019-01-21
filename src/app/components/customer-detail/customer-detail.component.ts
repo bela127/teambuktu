0c757de6f1b8a6e280ac7b0e716784283a3985ca
Matthieu Laqua <mlmatthieul@gmail.com>
2019-01-21 02:21:06 +0100

    Update customer detail view to only show completions in service history

--- src/app/components/customer-detail/customer-detail.component.ts
+++ src/app/components/customer-detail/customer-detail.component.ts
@@ -4,8 +4,8 @@ import {Customer} from "../../container/customer";
 import {CustomerService} from "../../services/customer.service";
 import {Device} from "../../container/device";
 import {DeviceService} from "../../services/device.service";
-import {Appointment} from "../../container/appointment";
-import {AppointmentService} from "../../services/appointment.service";
+import {Completion} from "../../container/completion";
+import {CompletionService} from "../../services/completion.service";
 
 @Component({
   selector: 'app-customer-detail',
@@ -16,11 +16,11 @@ export class CustomerDetailComponent implements OnInit {
 
   private customer: Customer;
   private customerDevices: Device[];
-  private customerAppointments: Appointment[];
+  private customerCompletions: Completion[];
 
   constructor(private customerService: CustomerService,
               private deviceService: DeviceService,
-              private appointmentService: AppointmentService,
+              private completionService: CompletionService,
               private route: ActivatedRoute) {
   }
 
@@ -37,10 +37,10 @@ export class CustomerDetailComponent implements OnInit {
                 .filter(d => d.customer == this.customer.id);
             });
 
-          this.appointmentService.getAppointments()
-            .subscribe(appointments => {
-              this.customerAppointments = appointments
-                .filter(a => a.customer == this.customer.id);
+          this.completionService.getCompletions()
+            .subscribe(completions => {
+              this.customerCompletions = completions
+                .filter(c => c.customer == this.customer.id);
             });
         });
     });
