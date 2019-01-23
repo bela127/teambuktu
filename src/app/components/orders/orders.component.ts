11cfa8d3c06f37e7a3961cfa0a056d9bf9455921
Matthieu Laqua <mlmatthieul@gmail.com>
2019-01-23 23:49:09 +0100

    Implement basic orders view

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Order} from "../../container/order";
+import {OrderService} from "../../services/order.service";
 
 @Component({
   selector: 'app-orders',
@@ -7,10 +9,24 @@ import {Component, OnInit} from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() {
+  private orders: Order[];
+
+  private displayedColumns = ['number', 'orderDate', 'lastUpdate', 'status', 'edit'];
+
+  constructor(private orderService: OrderService) {
   }
 
   ngOnInit() {
+    this.orderService.getOrders()
+      .subscribe(orders => this.orders = orders);
+  }
+
+  edit(order: Order): void {
+    console.log("Editing order " + JSON.stringify(order));
+  }
+
+  public add(): void {
+    console.log("Adding order");
   }
 
 }
